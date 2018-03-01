ae195ac80226f8347d086c68d4e22719af07290b
Nick Miyake <nmiyake@palantir.com>
2018-03-01 14:12:56 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+// Copyright 2016 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+
+	"github.com/palantir/amalgomate/amalgomated"
+	"github.com/palantir/okgo/checker"
+	"github.com/palantir/pkg/cobracli"
+
+	"github.com/palantir/godel-okgo-asset-errcheck/errcheck"
+	"github.com/palantir/godel-okgo-asset-errcheck/generated_src"
+)
+
+func main() {
+	os.Exit(amalgomated.RunApp(os.Args, nil, amalgomated.NewCmdLibrary(amalgomatedcheck.Instance()), checkMain))
+}
+
+func checkMain(osArgs []string) int {
+	os.Args = osArgs
+	var debugFlagVal bool
+	rootCmd := checker.AssetRootCmd(errcheck.Creator(), "run errcheck check")
+	return cobracli.ExecuteWithDefaultParamsWithVersion(rootCmd, &debugFlagVal, "")
+}
