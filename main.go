e58e32157e978d54ea0fd0e91df51607de302439
Nick Miyake <nmiyake@users.noreply.github.com>
2018-04-25 15:54:06 -0700

    Update cobra CLI version (#11)

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func main() {
 
 func checkMain(osArgs []string) int {
 	os.Args = osArgs
-	var debugFlagVal bool
 	rootCmd := checker.AssetRootCmd(creator.Errcheck(), config.UpgradeConfig, "run errcheck check")
-	return cobracli.ExecuteWithDefaultParamsWithVersion(rootCmd, &debugFlagVal, "")
+	return cobracli.ExecuteWithDefaultParams(rootCmd)
 }
